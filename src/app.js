8e1be67ca8b2ba3a4e59ac09732e13e8fe08f1d3
08EAGM94 <08edgar.allan94@gmail.com>
2026-03-16 16:51:07 -0600

    Enhance documentation in app.js
    Added comments to improve code documentation.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
+//The node:path module provides utilities for working with file and directory paths
 import path from "path";
 
-
+//express() is a top-level function exported by the express module in Node.js used to create an Express application instance.
 const app = express();
 
 //middleware
+//The app.use() function is used to mount middleware functions or routers to a specified path in the application's request-processing pipeline.
+//The path.join() method joins all given path segments together using the platform-specific separator as a delimiter, then normalizes the resulting path.
+//__dirname: It provides the absolute path to the directory of the current file
 app.use(express.static(path.join(__dirname, "public")));
 //
 
