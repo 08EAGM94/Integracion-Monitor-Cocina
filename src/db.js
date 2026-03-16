ba44c1bc5453d249b1112bf8c477303ee701f5ca
08EAGM94 <08edgar.allan94@gmail.com>
2026-03-16 17:13:40 -0600

    Document testConnection function with comments
    Added comments to explain the testConnection function.

--- src/db.js
+++ src/db.js
@@ -9,6 +9,11 @@ export const sequelize = new Sequelize(SQLSERVER_DB, SQLSERVER_USER, `${SQLSERVE
 
 export async function testConnection(){
     try{
+        /*
+        sequelize.authenticate() method is used to test if the connection to the database has been established successfully. 
+        It executes a simple query (e.g., SELECT 1+1 AS result) to verify that the configured credentials and settings are valid 
+        and that Sequelize can communicate with the database.
+        */
         await sequelize.authenticate();
         console.log("All good!!!");
     }catch(err){
