1544c31912a9be926b05e9a80da7f9560451612d
08EAGM94 <08edgar.allan94@gmail.com>
2026-03-16 18:12:02 -0600

    Add comments for table configuration in Clientes model
    Added comments to clarify table configuration in the Clientes model.

--- src/model/Clientes.model.js
+++ src/model/Clientes.model.js
@@ -42,9 +42,13 @@ Clientes.init({
         allowNull: true
     }
 },{
+    //se evita crear la pluralización del nombre de la tabla
     freezeTableName: true,
     sequelize,
+    //se trata de un modelo existente en la base de datos, aqui le indicamos a sequelize que 
+    //si busca la coincidencia que le indicamos no creará desde cero la tabla en cuestión
     modelName: "Clientes",
+    //evita que se registre una fecha de creación en la base de datos
     timestamps: false
 });
 
